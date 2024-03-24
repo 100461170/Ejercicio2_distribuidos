936b8b30775834e6528ada2938b62e6ce4d4ba9f
Tomas Mendizabal <100461170@alumnos.uc3m.es>
2024-03-24 17:17:31 +0100

    copied files from exercise 1

---
+++ servidor.h
@@ -0,0 +1,20 @@
+//
+// Created by linux-lex on 29/02/24.
+//
+#include "structures.h"
+
+#ifndef EJERCICIO1_DISTRIBUIDOS_SERVIDOR_H
+#define EJERCICIO1_DISTRIBUIDOS_SERVIDOR_H
+
+void * tratar_peticion(void *p);
+int s_init();
+int s_set_value(int key, char *valor1, int valor2_N, double *valor2_value);
+int s_get_value(int key, char *valor1, int * valor2_N, double *valor2_value);
+int s_modify_value(int key, char *valor1, int valor2_N, double *valor2_value);
+int s_delete_key(int key);
+int s_exist(int key);
+int load();
+int write_back();
+void close_server();
+
+#endif //EJERCICIO1_DISTRIBUIDOS_SERVIDOR_H
